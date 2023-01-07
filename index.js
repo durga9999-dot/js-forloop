06ccb6f10c1355aa8dd26a8f95d4618d372a2d45
durga9999-dot <84674883+durga9999-dot@users.noreply.github.com>
2023-01-07 06:08:27 +0530

    array sorting

--- index.js
+++ index.js
@@ -1,27 +1,695 @@
-// Import stylesheets
-import './style.css';
+// const numbers = [
+//   271, 245, 456, 687, 798, 122, 312, 698, 108, 524, 823, 914, 931,
+// ];
+const summary = [
+  {
+    trnInterval: '> 60',
+    paxCount: '28',
+    Trndate: '2021-08-26',
+    trnHour: '0',
+  },
+  {
+    trnInterval: '0-15',
+    paxCount: '69',
+    Trndate: '2021-08-26',
+    trnHour: '0',
+  },
+  {
+    trnInterval: '> 60',
+    paxCount: '36',
+    Trndate: '2021-08-26',
+    trnHour: '1',
+  },
+  {
+    trnInterval: '0-15',
+    paxCount: '56',
+    Trndate: '2021-08-26',
+    trnHour: '1',
+  },
+  {
+    trnInterval: '31-45',
+    paxCount: '1',
+    Trndate: '2021-08-26',
+    trnHour: '1',
+  },
+  {
+    trnInterval: '> 60',
+    paxCount: '40',
+    Trndate: '2021-08-26',
+    trnHour: '2',
+  },
+  {
+    trnInterval: '0-15',
+    paxCount: '53',
+    Trndate: '2021-08-26',
+    trnHour: '2',
+  },
+  {
+    trnInterval: '16-30',
+    paxCount: '1',
+    Trndate: '2021-08-26',
+    trnHour: '2',
+  },
+  {
+    trnInterval: '> 60',
+    paxCount: '81',
+    Trndate: '2021-08-26',
+    trnHour: '3',
+  },
+  {
+    trnInterval: '0-15',
+    paxCount: '47',
+    Trndate: '2021-08-26',
+    trnHour: '3',
+  },
+  {
+    trnInterval: '16-30',
+    paxCount: '25',
+    Trndate: '2021-08-26',
+    trnHour: '3',
+  },
+  {
+    trnInterval: '31-45',
+    paxCount: '32',
+    Trndate: '2021-08-26',
+    trnHour: '3',
+  },
+  {
+    trnInterval: '46-60',
+    paxCount: '41',
+    Trndate: '2021-08-26',
+    trnHour: '3',
+  },
+  {
+    trnInterval: '> 60',
+    paxCount: '102',
+    Trndate: '2021-08-26',
+    trnHour: '4',
+  },
+  {
+    trnInterval: '0-15',
+    paxCount: '113',
+    Trndate: '2021-08-26',
+    trnHour: '4',
+  },
+  {
+    trnInterval: '16-30',
+    paxCount: '83',
+    Trndate: '2021-08-26',
+    trnHour: '4',
+  },
+  {
+    trnInterval: '31-45',
+    paxCount: '68',
+    Trndate: '2021-08-26',
+    trnHour: '4',
+  },
+  {
+    trnInterval: '46-60',
+    paxCount: '37',
+    Trndate: '2021-08-26',
+    trnHour: '4',
+  },
+  {
+    trnInterval: '> 60',
+    paxCount: '119',
+    Trndate: '2021-08-26',
+    trnHour: '5',
+  },
+  {
+    trnInterval: '0-15',
+    paxCount: '140',
+    Trndate: '2021-08-26',
+    trnHour: '5',
+  },
+  {
+    trnInterval: '16-30',
+    paxCount: '104',
+    Trndate: '2021-08-26',
+    trnHour: '5',
+  },
+  {
+    trnInterval: '31-45',
+    paxCount: '122',
+    Trndate: '2021-08-26',
+    trnHour: '5',
+  },
+  {
+    trnInterval: '46-60',
+    paxCount: '85',
+    Trndate: '2021-08-26',
+    trnHour: '5',
+  },
+  {
+    trnInterval: '> 60',
+    paxCount: '117',
+    Trndate: '2021-08-26',
+    trnHour: '6',
+  },
+  {
+    trnInterval: '0-15',
+    paxCount: '141',
+    Trndate: '2021-08-26',
+    trnHour: '6',
+  },
+  {
+    trnInterval: '16-30',
+    paxCount: '121',
+    Trndate: '2021-08-26',
+    trnHour: '6',
+  },
+  {
+    trnInterval: '31-45',
+    paxCount: '126',
+    Trndate: '2021-08-26',
+    trnHour: '6',
+  },
+  {
+    trnInterval: '46-60',
+    paxCount: '87',
+    Trndate: '2021-08-26',
+    trnHour: '6',
+  },
+  {
+    trnInterval: '> 60',
+    paxCount: '147',
+    Trndate: '2021-08-26',
+    trnHour: '7',
+  },
+  {
+    trnInterval: '0-15',
+    paxCount: '130',
+    Trndate: '2021-08-26',
+    trnHour: '7',
+  },
+  {
+    trnInterval: '16-30',
+    paxCount: '61',
+    Trndate: '2021-08-26',
+    trnHour: '7',
+  },
+  {
+    trnInterval: '31-45',
+    paxCount: '68',
+    Trndate: '2021-08-26',
+    trnHour: '7',
+  },
+  {
+    trnInterval: '46-60',
+    paxCount: '101',
+    Trndate: '2021-08-26',
+    trnHour: '7',
+  },
+  {
+    trnInterval: '> 60',
+    paxCount: '144',
+    Trndate: '2021-08-26',
+    trnHour: '8',
+  },
+  {
+    trnInterval: '0-15',
+    paxCount: '146',
+    Trndate: '2021-08-26',
+    trnHour: '8',
+  },
+  {
+    trnInterval: '16-30',
+    paxCount: '79',
+    Trndate: '2021-08-26',
+    trnHour: '8',
+  },
+  {
+    trnInterval: '31-45',
+    paxCount: '95',
+    Trndate: '2021-08-26',
+    trnHour: '8',
+  },
+  {
+    trnInterval: '46-60',
+    paxCount: '101',
+    Trndate: '2021-08-26',
+    trnHour: '8',
+  },
+  {
+    trnInterval: '> 60',
+    paxCount: '137',
+    Trndate: '2021-08-26',
+    trnHour: '9',
+  },
+  {
+    trnInterval: '0-15',
+    paxCount: '130',
+    Trndate: '2021-08-26',
+    trnHour: '9',
+  },
+  {
+    trnInterval: '16-30',
+    paxCount: '97',
+    Trndate: '2021-08-26',
+    trnHour: '9',
+  },
+  {
+    trnInterval: '31-45',
+    paxCount: '116',
+    Trndate: '2021-08-26',
+    trnHour: '9',
+  },
+  {
+    trnInterval: '46-60',
+    paxCount: '85',
+    Trndate: '2021-08-26',
+    trnHour: '9',
+  },
+  {
+    trnInterval: '> 60',
+    paxCount: '129',
+    Trndate: '2021-08-26',
+    trnHour: '10',
+  },
+  {
+    trnInterval: '0-15',
+    paxCount: '99',
+    Trndate: '2021-08-26',
+    trnHour: '10',
+  },
+  {
+    trnInterval: '16-30',
+    paxCount: '58',
+    Trndate: '2021-08-26',
+    trnHour: '10',
+  },
+  {
+    trnInterval: '31-45',
+    paxCount: '39',
+    Trndate: '2021-08-26',
+    trnHour: '10',
+  },
+  {
+    trnInterval: '46-60',
+    paxCount: '25',
+    Trndate: '2021-08-26',
+    trnHour: '10',
+  },
+  {
+    trnInterval: '> 60',
+    paxCount: '123',
+    Trndate: '2021-08-26',
+    trnHour: '11',
+  },
+  {
+    trnInterval: '0-15',
+    paxCount: '118',
+    Trndate: '2021-08-26',
+    trnHour: '11',
+  },
+  {
+    trnInterval: '16-30',
+    paxCount: '65',
+    Trndate: '2021-08-26',
+    trnHour: '11',
+  },
+  {
+    trnInterval: '31-45',
+    paxCount: '64',
+    Trndate: '2021-08-26',
+    trnHour: '11',
+  },
+  {
+    trnInterval: '46-60',
+    paxCount: '65',
+    Trndate: '2021-08-26',
+    trnHour: '11',
+  },
+  {
+    trnInterval: '> 60',
+    paxCount: '125',
+    Trndate: '2021-08-26',
+    trnHour: '12',
+  },
+  {
+    trnInterval: '0-15',
+    paxCount: '159',
+    Trndate: '2021-08-26',
+    trnHour: '12',
+  },
+  {
+    trnInterval: '16-30',
+    paxCount: '54',
+    Trndate: '2021-08-26',
+    trnHour: '12',
+  },
+  {
+    trnInterval: '31-45',
+    paxCount: '72',
+    Trndate: '2021-08-26',
+    trnHour: '12',
+  },
+  {
+    trnInterval: '46-60',
+    paxCount: '94',
+    Trndate: '2021-08-26',
+    trnHour: '12',
+  },
+  {
+    trnInterval: '> 60',
+    paxCount: '136',
+    Trndate: '2021-08-26',
+    trnHour: '13',
+  },
+  {
+    trnInterval: '0-15',
+    paxCount: '163',
+    Trndate: '2021-08-26',
+    trnHour: '13',
+  },
+  {
+    trnInterval: '16-30',
+    paxCount: '78',
+    Trndate: '2021-08-26',
+    trnHour: '13',
+  },
+  {
+    trnInterval: '31-45',
+    paxCount: '88',
+    Trndate: '2021-08-26',
+    trnHour: '13',
+  },
+  {
+    trnInterval: '46-60',
+    paxCount: '65',
+    Trndate: '2021-08-26',
+    trnHour: '13',
+  },
+  {
+    trnInterval: '> 60',
+    paxCount: '154',
+    Trndate: '2021-08-26',
+    trnHour: '14',
+  },
+  {
+    trnInterval: '0-15',
+    paxCount: '82',
+    Trndate: '2021-08-26',
+    trnHour: '14',
+  },
+  {
+    trnInterval: '16-30',
+    paxCount: '44',
+    Trndate: '2021-08-26',
+    trnHour: '14',
+  },
+  {
+    trnInterval: '31-45',
+    paxCount: '91',
+    Trndate: '2021-08-26',
+    trnHour: '14',
+  },
+  {
+    trnInterval: '46-60',
+    paxCount: '92',
+    Trndate: '2021-08-26',
+    trnHour: '14',
+  },
+  {
+    trnInterval: '> 60',
+    paxCount: '154',
+    Trndate: '2021-08-26',
+    trnHour: '15',
+  },
+  {
+    trnInterval: '0-15',
+    paxCount: '121',
+    Trndate: '2021-08-26',
+    trnHour: '15',
+  },
+  {
+    trnInterval: '16-30',
+    paxCount: '94',
+    Trndate: '2021-08-26',
+    trnHour: '15',
+  },
+  {
+    trnInterval: '31-45',
+    paxCount: '100',
+    Trndate: '2021-08-26',
+    trnHour: '15',
+  },
+  {
+    trnInterval: '46-60',
+    paxCount: '75',
+    Trndate: '2021-08-26',
+    trnHour: '15',
+  },
+  {
+    trnInterval: '> 60',
+    paxCount: '180',
+    Trndate: '2021-08-26',
+    trnHour: '16',
+  },
+  {
+    trnInterval: '0-15',
+    paxCount: '107',
+    Trndate: '2021-08-26',
+    trnHour: '16',
+  },
+  {
+    trnInterval: '16-30',
+    paxCount: '33',
+    Trndate: '2021-08-26',
+    trnHour: '16',
+  },
+  {
+    trnInterval: '31-45',
+    paxCount: '57',
+    Trndate: '2021-08-26',
+    trnHour: '16',
+  },
+  {
+    trnInterval: '46-60',
+    paxCount: '77',
+    Trndate: '2021-08-26',
+    trnHour: '16',
+  },
+  {
+    trnInterval: '> 60',
+    paxCount: '171',
+    Trndate: '2021-08-26',
+    trnHour: '17',
+  },
+  {
+    trnInterval: '0-15',
+    paxCount: '120',
+    Trndate: '2021-08-26',
+    trnHour: '17',
+  },
+  {
+    trnInterval: '16-30',
+    paxCount: '83',
+    Trndate: '2021-08-26',
+    trnHour: '17',
+  },
+  {
+    trnInterval: '31-45',
+    paxCount: '113',
+    Trndate: '2021-08-26',
+    trnHour: '17',
+  },
+  {
+    trnInterval: '46-60',
+    paxCount: '100',
+    Trndate: '2021-08-26',
+    trnHour: '17',
+  },
+  {
+    trnInterval: '> 60',
+    paxCount: '140',
+    Trndate: '2021-08-26',
+    trnHour: '18',
+  },
+  {
+    trnInterval: '0-15',
+    paxCount: '78',
+    Trndate: '2021-08-26',
+    trnHour: '18',
+  },
+  {
+    trnInterval: '16-30',
+    paxCount: '66',
+    Trndate: '2021-08-26',
+    trnHour: '18',
+  },
+  {
+    trnInterval: '31-45',
+    paxCount: '98',
+    Trndate: '2021-08-26',
+    trnHour: '18',
+  },
+  {
+    trnInterval: '46-60',
+    paxCount: '80',
+    Trndate: '2021-08-26',
+    trnHour: '18',
+  },
+  {
+    trnInterval: '> 60',
+    paxCount: '134',
+    Trndate: '2021-08-26',
+    trnHour: '19',
+  },
+  {
+    trnInterval: '0-15',
+    paxCount: '118',
+    Trndate: '2021-08-26',
+    trnHour: '19',
+  },
+  {
+    trnInterval: '16-30',
+    paxCount: '76',
+    Trndate: '2021-08-26',
+    trnHour: '19',
+  },
+  {
+    trnInterval: '31-45',
+    paxCount: '82',
+    Trndate: '2021-08-26',
+    trnHour: '19',
+  },
+  {
+    trnInterval: '46-60',
+    paxCount: '71',
+    Trndate: '2021-08-26',
+    trnHour: '19',
+  },
+  {
+    trnInterval: '> 60',
+    paxCount: '108',
+    Trndate: '2021-08-26',
+    trnHour: '20',
+  },
+  {
+    trnInterval: '0-15',
+    paxCount: '111',
+    Trndate: '2021-08-26',
+    trnHour: '20',
+  },
+  {
+    trnInterval: '16-30',
+    paxCount: '26',
+    Trndate: '2021-08-26',
+    trnHour: '20',
+  },
+  {
+    trnInterval: '31-45',
+    paxCount: '39',
+    Trndate: '2021-08-26',
+    trnHour: '20',
+  },
+  {
+    trnInterval: '46-60',
+    paxCount: '51',
+    Trndate: '2021-08-26',
+    trnHour: '20',
+  },
+  {
+    trnInterval: '> 60',
+    paxCount: '12',
+    Trndate: '2021-08-26',
+    trnHour: '21',
+  },
+  {
+    trnInterval: '0-15',
+    paxCount: '68',
+    Trndate: '2021-08-26',
+    trnHour: '21',
+  },
+  {
+    trnInterval: '16-30',
+    paxCount: '49',
+    Trndate: '2021-08-26',
+    trnHour: '21',
+  },
+  {
+    trnInterval: '31-45',
+    paxCount: '27',
+    Trndate: '2021-08-26',
+    trnHour: '21',
+  },
+  {
+    trnInterval: '46-60',
+    paxCount: '28',
+    Trndate: '2021-08-26',
+    trnHour: '21',
+  },
+  {
+    trnInterval: '0-15',
+    paxCount: '33',
+    Trndate: '2021-08-26',
+    trnHour: '22',
+  },
+  {
+    trnInterval: '16-30',
+    paxCount: '21',
+    Trndate: '2021-08-26',
+    trnHour: '22',
+  },
+  {
+    trnInterval: '31-45',
+    paxCount: '6',
+    Trndate: '2021-08-26',
+    trnHour: '22',
+  },
+];
 
-// Write Javascript code!
+// summary.sort((e1,e2) =>{
+//   if(e1.trnInterval < e2.trnInterval) return-1;
+//   if(e1.trnInterval < e2.trnInterval) return 1;
+//   return 0;
+// })
+// for(let sum of summary){
+//   console.log(sum.trnInterval)
+// }
+
+summary.sort((e1, e2) => {
+  if (e1.paxCount < e2.paxCount) return -1;
+  if (e1.paxCount < e2.paxCount) return 1;
+  return 0;
+});
+for (sum of summary) {
+  console.log(sum.paxCount);
+}
+
+// for (let i = 0; i < numbers.length; i++) {
+//   console.log(i, numbers[i]);
+// }
+
+// // const numbers = [29, 45, 56, 87, 98, 22, 12, 98, 10];
+// for (const idx in numbers) {
+//   console.log(numbers[idx]);
+// }
+
+// // const numbers = [23, 45, 56, 87, 98, 22, 12, 98, 10];
+
+// let idx = 0;
+// for (const num of numbers) {
+//   console.log(idx, num);
+//   ++idx;
+// }
+
+// // const numbers = [23, 45, 56, 87, 98, 22, 12, 98, 10, 24, 23, 14, 31];
+// // numbers.forEach((num, idx) => {
+// //   console.log(idx, numbers);
+// // });
+
+// summary.forEach((sum, index) => {
+//   console.log(summary);
+// });
+// const number = [23, 45, 56, 87, 98, 22, 12, 98, 10];
+
+// let data = number.reduce((acc, cur) => acc + cur);
+
+// console.log(data);
+
 const numbers = [23, 45, 56, 87, 98, 22, 12, 98, 10];
-
-for (let i = 0; i < numbers.length; i++) {
-  console.log(i, numbers[i]);
-}
-
-// const numbers = [29, 45, 56, 87, 98, 22, 12, 98, 10];
-for (const idx in numbers) {
-  console.log(numbers[idx]);
-}
-
-// const numbers = [23, 45, 56, 87, 98, 22, 12, 98, 10];
-
-let idx = 0;
-for (const num of numbers) {
+const data = numbers.forEach((num, idx) => {
   console.log(idx, num);
-  ++idx;
-}
-
-// const numbers = [23, 45, 56, 87, 98, 22, 12, 98, 10, 24, 23, 14, 31];
-numbers.forEach((num, idx) => {
-  console.log(idx, numbers);
 });
